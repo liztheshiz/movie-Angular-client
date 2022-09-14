113169542164ce0b7bf0a932d68db38f50aacd34
liztheshiz <65947305+liztheshiz@users.noreply.github.com>
2022-09-14 11:42:44 -0400

    implemented userloginform based on userregistrationform format

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
+import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
 import { MatDialog } from '@angular/material/dialog';
 
 @Component({
@@ -11,10 +12,18 @@ export class AppComponent {
     title = 'movie-Angular-client';
 
     constructor(public dialog: MatDialog) { }
-    // Opens the dialog when the signup button is clicked  
+
+    // Opens the registration dialog when the signup button is clicked  
     openUserRegistrationDialog(): void {
         this.dialog.open(UserRegistrationFormComponent, {
             // Assigning the dialog a width
+            width: '280px'
+        });
+    }
+
+    // Opens the login dialog when the login button is clicked
+    openUserLoginDialog(): void {
+        this.dialog.open(UserLoginFormComponent, {
             width: '280px'
         });
     }
--- src/app/user-login-form/user-login-form.component.ts
+++ src/app/user-login-form/user-login-form.component.ts
@@ -1,15 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
+import { FetchApiDataService } from '../fetch-api-data.service';
+// Displays notifications back to the user
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
-  selector: 'app-user-login-form',
-  templateUrl: './user-login-form.component.html',
-  styleUrls: ['./user-login-form.component.scss']
+    selector: 'app-user-login-form',
+    templateUrl: './user-login-form.component.html',
+    styleUrls: ['./user-login-form.component.scss']
 })
 export class UserLoginFormComponent implements OnInit {
 
-  constructor() { }
+    // Defines component's input
+    @Input() userData = { Username: '', Password: '' }
 
-  ngOnInit(): void {
-  }
+    constructor(
+        public fetchApiData: FetchApiDataService,
+        public dialogRef: MatDialogRef<UserLoginFormComponent>,
+        public snackBar: MatSnackBar) { }
 
+    // Called when inputs recieve their data (from user)
+    ngOnInit(): void {
+    }
+
+    // Passes userData object into API
+    loginUser(): void {
+        this.fetchApiData.userLogin(this.userData).subscribe((result) => {
+            // Logic for a successful user login goes here! (To be implemented)
+            localStorage.setItem('user', result.user.Username);
+            localStorage.setItem('token', result.token);
+            this.dialogRef.close(); // This will close the modal on success!
+            console.log(result);
+            this.snackBar.open(result, 'OK', {
+                duration: 2000
+            });
+        }, (result) => {
+            console.log(result);
+            this.snackBar.open(result, 'OK', {
+                duration: 2000
+            });
+        });
+    }
 }
