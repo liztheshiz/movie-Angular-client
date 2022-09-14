5de2ec99f90c3d1d778091971227e44daa7addef
liztheshiz <65947305+liztheshiz@users.noreply.github.com>
2022-09-14 09:51:37 -0400

    imported components into app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,15 @@ import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+// Material components
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { FormsModule } from '@angular/forms';
+
 @NgModule({
     declarations: [
         AppComponent
@@ -15,7 +24,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
         BrowserModule,
         AppRoutingModule,
         HttpClientModule,
-        BrowserAnimationsModule
+        BrowserAnimationsModule,
+        FormsModule,
+        MatInputModule,
+        MatButtonModule,
+        MatCardModule,
+        MatFormFieldModule,
+        MatDialogModule,
+        MatSnackBarModule
     ],
     providers: [],
     bootstrap: [AppComponent]
