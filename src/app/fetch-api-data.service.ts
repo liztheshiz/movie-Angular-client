9399cef78bf87ff465e45b5f345ac29ac2625de8
liztheshiz <65947305+liztheshiz@users.noreply.github.com>
2022-09-14 11:21:52 -0400

    fixed api call typo for login

--- src/app/fetch-api-data.service.ts
+++ src/app/fetch-api-data.service.ts
@@ -37,7 +37,7 @@ export class FetchApiDataService {
     // Logs existing user in
     public userLogin(userDetails: any): Observable<any> {
         console.log(userDetails);
-        return this.http.post(apiUrl + 'users', userDetails).pipe(
+        return this.http.post(apiUrl + 'login', userDetails).pipe(
             catchError(this.handleError)
         );
     }
