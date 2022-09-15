dbf5a0b728c140bfe3b6a1198d1380d165ec8d10
liztheshiz <65947305+liztheshiz@users.noreply.github.com>
2022-09-15 13:42:43 -0400

    moved main page from root to welcome-page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,4 @@
 import { Component } from '@angular/core';
-import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
-import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
-import { MatDialog } from '@angular/material/dialog';
-import { MovieCardComponent } from './movie-card/movie-card.component';
 
 @Component({
     selector: 'app-root',
@@ -10,28 +6,5 @@ import { MovieCardComponent } from './movie-card/movie-card.component';
     styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-    title = 'movie-Angular-client';
-
-    constructor(public dialog: MatDialog) { }
-
-    // Opens the registration dialog when the signup button is clicked  
-    openUserRegistrationDialog(): void {
-        this.dialog.open(UserRegistrationFormComponent, {
-            // Assigning the dialog a width
-            width: '280px'
-        });
-    }
-
-    // Opens the login dialog when the login button is clicked
-    openUserLoginDialog(): void {
-        this.dialog.open(UserLoginFormComponent, {
-            width: '280px'
-        });
-    }
-
-    openMoviesDialog(): void {
-        this.dialog.open(MovieCardComponent, {
-            width: '500px'
-        });
-    }
+    title = 'myFlix-Angular-client';
 }
