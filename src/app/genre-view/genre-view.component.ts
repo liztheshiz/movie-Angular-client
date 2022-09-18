aaf61d28aab4dc55c57b57b7f403362efbd0b7bf
liztheshiz <65947305+liztheshiz@users.noreply.github.com>
2022-09-18 11:27:09 -0400

    passes data to genre-view

--- src/app/genre-view/genre-view.component.ts
+++ src/app/genre-view/genre-view.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Inject } from '@angular/core';
+
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+
 @Component({
-  selector: 'app-genre-view',
-  templateUrl: './genre-view.component.html',
-  styleUrls: ['./genre-view.component.scss']
+    selector: 'app-genre-view',
+    templateUrl: './genre-view.component.html',
+    styleUrls: ['./genre-view.component.scss']
 })
 export class GenreViewComponent implements OnInit {
 
-  constructor() { }
+    constructor(@Inject(MAT_DIALOG_DATA) public data: { Genre: { Name: String, Description: String } }) { }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
 
 }
--- src/app/movie-card/movie-card.component.ts
+++ src/app/movie-card/movie-card.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FetchApiDataService } from '../fetch-api-data.service';
 
 import { DirectorViewComponent } from '../director-view/director-view.component';
+import { GenreViewComponent } from '../genre-view/genre-view.component';
 import { MatDialog } from '@angular/material/dialog';
 
 @Component({
