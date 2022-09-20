1482727eb387b67fc40ede5a74451800bf3cff1f
liztheshiz <65947305+liztheshiz@users.noreply.github.com>
2022-09-20 11:18:14 -0400

    added navigation to profile view

--- src/app/profile-view/profile-view.component.ts
+++ src/app/profile-view/profile-view.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 
+import { FetchApiDataService } from '../fetch-api-data.service';
+
+import { Router } from '@angular/router';
+
 @Component({
-  selector: 'app-profile-view',
-  templateUrl: './profile-view.component.html',
-  styleUrls: ['./profile-view.component.scss']
+    selector: 'app-profile-view',
+    templateUrl: './profile-view.component.html',
+    styleUrls: ['./profile-view.component.scss']
 })
 export class ProfileViewComponent implements OnInit {
+    user: any = localStorage.getItem('user');
+    constructor(public fetchApiData: FetchApiDataService, public router: Router) { }
 
-  constructor() { }
+    ngOnInit(): void {
+    }
 
-  ngOnInit(): void {
-  }
+    openMovieView(): void {
+        this.router.navigate(['movies']);
+    }
 
+    logout(): void {
+        localStorage.removeItem('user');
+        localStorage.removeItem('token');
+        this.router.navigate(['welcome']);
+    }
 }
