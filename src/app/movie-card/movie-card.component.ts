da420594d10bdcad6be7fb863e35c56261b05fb8
liztheshiz <65947305+liztheshiz@users.noreply.github.com>
2022-09-20 10:51:59 -0400

    implemented synopsis card

--- src/app/movie-card/movie-card.component.ts
+++ src/app/movie-card/movie-card.component.ts
@@ -3,6 +3,7 @@ import { FetchApiDataService } from '../fetch-api-data.service';
 
 import { DirectorViewComponent } from '../director-view/director-view.component';
 import { GenreViewComponent } from '../genre-view/genre-view.component';
+import { SynopsisViewComponent } from '../synopsis-view/synopsis-view.component';
 import { MatDialog } from '@angular/material/dialog';
 
 import { Router } from '@angular/router';
@@ -72,6 +73,15 @@ export class MovieCardComponent implements OnInit {
         this.dialog.open(GenreViewComponent, {
             data: {
                 Genre: genre
+            },
+            width: '280px'
+        });
+    }
+
+    openSynopsisView(movie: Object): void {
+        this.dialog.open(SynopsisViewComponent, {
+            data: {
+                Movie: movie
             },
             width: '280px'
         });
--- src/app/synopsis-view/synopsis-view.component.ts
+++ src/app/synopsis-view/synopsis-view.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Inject } from '@angular/core';
+
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+
 @Component({
-  selector: 'app-synopsis-view',
-  templateUrl: './synopsis-view.component.html',
-  styleUrls: ['./synopsis-view.component.scss']
+    selector: 'app-synopsis-view',
+    templateUrl: './synopsis-view.component.html',
+    styleUrls: ['./synopsis-view.component.scss']
 })
 export class SynopsisViewComponent implements OnInit {
 
-  constructor() { }
+    constructor(@Inject(MAT_DIALOG_DATA) public data: { Movie: { Title: String, Description: String } }) { }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
 
 }
