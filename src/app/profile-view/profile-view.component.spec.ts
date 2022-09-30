e1ce95d6bb55ad1dc51646401157ae3b82bf6c05
liztheshiz <65947305+liztheshiz@users.noreply.github.com>
2022-09-30 13:51:36 -0400

    saved and automatically adjusted spacing

--- src/app/profile-view/profile-view.component.spec.ts
+++ src/app/profile-view/profile-view.component.spec.ts
@@ -3,21 +3,21 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ProfileViewComponent } from './profile-view.component';
 
 describe('ProfileViewComponent', () => {
-  let component: ProfileViewComponent;
-  let fixture: ComponentFixture<ProfileViewComponent>;
+    let component: ProfileViewComponent;
+    let fixture: ComponentFixture<ProfileViewComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ ProfileViewComponent ]
-    })
-    .compileComponents();
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [ProfileViewComponent]
+        })
+            .compileComponents();
 
-    fixture = TestBed.createComponent(ProfileViewComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+        fixture = TestBed.createComponent(ProfileViewComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
